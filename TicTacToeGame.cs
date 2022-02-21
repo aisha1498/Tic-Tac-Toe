326da52e6a233e056e320701e216bcee5279fef0
aisha1498 <95531968+aisha1498@users.noreply.github.com>
2022-02-21 10:39:50 +0530

    Add files via upload

---
+++ TicTacToeGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class TicTacToeGame
+    {
+        static void Main(string[] args)
+        {
+            var stillPlaying = true;
+            Console.WriteLine("Welcome to Tic Tac Toe Game.");
+
+            while (stillPlaying)
+            {
+                Console.WriteLine("What woulde you like to do?");
+                Console.WriteLine("1. Start a new Game\n");
+                Console.WriteLine("2. Quite");
+                Console.WriteLine(" Type a number and <enter>:");
+            }
+            //making array
+          char[] arr = { ' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+          
+
+        }
+    }
+}
